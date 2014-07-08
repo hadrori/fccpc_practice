2c84e73d9fb6622acdfc8f3642c5194f5c09d329
John Doe <isozuka.k.aa@m.titech.ac.jp>
2014-07-08 19:15:51 +0900

    Update templates

--- lite.cpp
+++ lite.cpp
@@ -16,19 +16,11 @@
 
 #define rep(i,n) repi(i,0,n)
 #define repi(i,a,b) for(int i=(int)(a);i<(int)(b);++i)
-#define repd(i,a,b) for(int i=(int)(a);i>=(int)(b);--i)
-#define repit(it,u) for(__typeof((u).begin()) it=(u).begin();it!=(u).end();++it)
-
-#define all(u) (u).begin(),(u).end()
-#define rall(u) (u).rbegin(),(u).rend()
-#define uniq(u) (u).erase(unique(all(u)),(u).end())
 
 #define pb push_back
 #define mp make_pair
 
 using namespace std;
-
-typedef long long ll;
 
 int main()
 {
