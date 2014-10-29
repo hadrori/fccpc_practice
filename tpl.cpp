7d31f8187d640b2e8592c58bb7ed22710c41c6f1
John Doe <isozuka.k.aa@m.titech.ac.jp>
2014-10-29 14:38:29 +0900

    Update tpl.cpp

--- tpl.cpp
+++ tpl.cpp
@@ -1,29 +1,14 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <cctype>
-#include <cmath>
-#include <complex>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 #define rep(i,n) repi(i,0,n)
 #define repi(i,a,b) for(int i=(int)(a);i<(int)(b);++i)
 #define repd(i,a,b) for(int i=(int)(a);i>=(int)(b);--i)
-#define repit(it,u) for(__typeof((u).begin()) it=(u).begin();it!=(u).end();++it)
+#define repit(it,u) for(auto it=begin(u);it!=end(u);++it)
 
-#define all(u) (u).begin(),(u).end()
+#define all(u) begin(u),end(u)
 #define rall(u) (u).rbegin(),(u).rend()
-#define uniq(u) (u).erase(unique(all(u)),(u).end())
+#define uniq(u) (u).erase(unique(all(u)),end(u))
 
 #define pb push_back
 #define mp make_pair
